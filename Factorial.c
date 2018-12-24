7062a8bb413248683d9553f817a4ee800471cc05
divyamiglani <34910877+divyamiglani@users.noreply.github.com>
2018-12-24 12:39:20 +0530

    Update Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+int fact(int n)
+{
+     if (n >=1)
+        return(n * fact(n-1));
+     else
+        return 1;
+}
+         
 int main()
 {
     int n, i;
@@ -8,19 +16,11 @@ int main()
 
     printf("Enter a number: ");
     scanf("%d",&n);
-
-    // Show error if number is less than 0
-    if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.");
-
-    else
-    {
-        for(i=1; i<=n; ++i)
-        {
-            factorial *= i;              // factorial = factorial*i;
-        }
-        printf("Factorial of %d = %llu", n, factorial);
-    }
-
+    factorial = fact(n);
+    printf("%d",factorial);
+    
     return 0;
 }
+
+
+   
